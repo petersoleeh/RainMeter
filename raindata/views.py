60a3be4a1aa5c1e40281dbfd0506def9fcf306ac
peter soleeh <petersoleeh@gmail.com>
2018-05-02 13:22:58 +0300

    Layout the index page and setup views for the form

--- raindata/views.py
+++ raindata/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from .models import Raindata
+from .forms import RainForm
 
 # Create your views here.
 def index(request):
     data = Raindata.objects.all()
+    # graph = Raindata.rain_graph()
 
-    return render(request,'index.html',{'data':data})
+    if request.method == 'POST':
+        form = RainForm(request.POST)
+        if form.is_valid():
+            raindata = form.save(commit=False)
+            raindata.save()
+
+    else:
+
+        form = RainForm()
+
+    return render(request,'index.html',{'data':data,'form':form})
